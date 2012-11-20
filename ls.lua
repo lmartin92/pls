8fd392b4205c7cbf5daf2397b78c12fb95f10f0e
Lester Martin II <nitram@localhost.localdomain>
2012-11-19 21:52:26 -0600

    Changed the source to include getopt (no need to have in special place) started adding comments, authors of various things credited where needed

--- ls.lua
+++ ls.lua
@@ -17,9 +17,43 @@
 --          plugin options :
 --              any left over arguments are used by the plugin
 
+-- require all we need, lfs for lfs.dir, args for argument parsing
+-- and pl.path for ~ expansion
 local lfs   = require "lfs"
-local args  = require "args"
 local path  = require "pl.path"
+
+-- this is getopt as written by ShmuelZeigerman
+-- copied from http://lua-users.org/wiki/AlternativeGetOpt
+function getopt( arg, options )
+  local tab = {}
+  for k, v in ipairs(arg) do
+    if string.sub( v, 1, 2) == "--" then
+      local x = string.find( v, "=", 1, true )
+      if x then tab[ string.sub( v, 3, x-1 ) ] = string.sub( v, x+1 )
+      else      tab[ string.sub( v, 3 ) ] = true
+      end
+    elseif string.sub( v, 1, 1 ) == "-" then
+      local y = 2
+      local l = string.len(v)
+      local jopt
+      while ( y <= l ) do
+        jopt = string.sub( v, y, y )
+        if string.find( options, jopt, 1, true ) then
+          if y < l then
+            tab[ jopt ] = string.sub( v, y+1 )
+            y = l
+          else
+            tab[ jopt ] = arg[ k + 1 ]
+          end
+        else
+          tab[ jopt ] = true
+        end
+        y = y + 1
+      end
+    end
+  end
+  return tab
+end 
 
 function require_args(args, ...)
     ret = true
